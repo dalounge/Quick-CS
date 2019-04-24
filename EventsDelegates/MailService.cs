55111bf8a4de10e01a7ffc55ac42c4bf88126cce
Dale Nguyen <41927798+dalounge@users.noreply.github.com>
2019-04-24 17:04:08 -0500

    Create MailService.cs

---
+++ EventsDelegates/MailService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelgate
+{
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e)
+        {
+            Console.WriteLine($"MailService:  Sending an email...{e.Video.Title}");
+        }
+    }
+    
+}
